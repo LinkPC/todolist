18cf642eef8de8ac60158111cabf8deeba5dfe0a
LinkPC <hpradoc1400@alumno.ipn.mx>
2022-01-07 12:51:39 -0600

    typesOfClasses

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,13 @@
 <template>
 <div class="home">
   <p>¡Hola vue! uwu</p>
+  <p class="red blue--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus aspernatur, ullam incidunt impedit voluptas dicta obcaecati nemo qui sit, nihil odio, modi sapiente recusandae consequuntur illum explicabo corporis enim sunt!</p>
+  <p class="pink lighten-4 red--text text--darken-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur, quod rem nulla quae animi non quasi unde atque eius expedita doloremque libero, at, laboriosam ab aut odit dolor recusandae beatae.</p>
+  <h1 class="display-4">Massive Display</h1>
+  <h4 class="display-1">Smaller display</h4>
+  <p class="headline">This is headline</p>
+  <p class="subheading">This is a subheading</p>
+  <p class="caption">Caption</p>
 </div>
 </template>
 
