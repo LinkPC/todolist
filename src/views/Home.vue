648cf7cfa1258bc5f59e69be7d21123fb737c581
LinkPC <hpradoc1400@alumno.ipn.mx>
2022-01-10 08:37:23 -0600

    BreakpointsVisibile

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,28 +2,8 @@
 <div class="home">
   <p>¡Hola vue! uwu</p>
   
-  <v-btn class="pink white--text">click me</v-btn>
-  <v-btn depressed color="pink">click</v-btn>
-  <v-btn text color="pink">click</v-btn>
+  <v-btn class="d-lg-none d-xl-flex">click me</v-btn>
 
-  <v-btn depressed class="pink white--text">
-    <v-icon left> mdi-train </v-icon>
-    <span>mugen</span>
-  </v-btn>
-
-  <v-btn depressed small class="pink white--text">
-    <v-icon left small> mdi-train </v-icon>
-    <span>mugen</span>
-  </v-btn>
-
-  <v-btn depressed large class="pink white--text">
-    <span>mugen</span>
-    <v-icon right large> mdi-train </v-icon>
-  </v-btn>
-
-  <v-btn fab depressed small dark color="purple">
-    <v-icon> mdi-one-up </v-icon>
-  </v-btn>
 </div>
 </template>
 
